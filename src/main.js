fa0c2ca9554b03190138255af5995bf7ea9be952
testishe <32316231+testishe@users.noreply.github.com>
2020-06-21 18:47:28 +0500

    full project

---
+++ src/main.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
+import VueMeta from 'vue-meta'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import dateFilter from '@/filters/date.filter'
+import currencyFilter from '@/filters/currency.filter'
+import localizeFilter from '@/filters/localize.filter'
+import tooltipDirective from '@/directives/tooltip.directive'
+import messagePlugin from '@/utils/message.plugin'
+import titlePlugin from '@/utils/title.plugin'
+import Loader from '@/components/app/Loader'
+import './registerServiceWorker'
+import 'materialize-css/dist/js/materialize.min'
+
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+
+Vue.config.productionTip = false
+
+Vue.use(messagePlugin)
+Vue.use(titlePlugin)
+Vue.use(Vuelidate)
+Vue.use(VueMeta)
+Vue.filter('date', dateFilter)
+Vue.filter('localize', localizeFilter)
+Vue.filter('currency', currencyFilter)
+Vue.directive('tooltip', tooltipDirective)
+Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyADVat1jwwZm5OHbC5lydl697P1G432L9c',
+  authDomain: 'learn-crm.firebaseapp.com',
+  databaseURL: 'https://learn-crm.firebaseio.com',
+  projectId: 'learn-crm',
+  storageBucket: 'learn-crm.appspot.com',
+  messagingSenderId: '23908761969',
+  appId: '1:23908761969:web:a69f9e27e9a678396a76d0',
+  measurementId: 'G-SYHDYKW9CK'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
